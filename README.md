cdb2417404a94e352229a6fa641fcc789c1f7926
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-09-26 14:08:45 +0530

    Complete TODO Tasks APP

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
-# Todo_List
-Sharing code
+# This is Simple ToDo Task App.
 
-This is Simple ToDo Task App.
+```
+Although it's a frontened project but you never gonna loss your tasks that your created even after refressing page because In This Project I'm using localStrorage for storing tasks text.
+```
 <h2> 
-   <a href = "https://rks107.github.io/Todo_List/" > Click Here </a>
-   to visite this App.
+   <a href = "https://rks107.github.io/TODO_APP/" > Click Here </a>
+   to visite this TODO Tasks App.
 </h2>
-<a href = "https://rks107.github.io/Todo_List/" >
+<a href = "https://rks107.github.io/TODO_APP/" >
 <img src="https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2018/06/Screen-Shot-2018-06-13-at-10.31.06-PM.png">
 </a>
+
+
+# For Cloning this Frontened project
+
+```
+git clone https://github.com/rks107/react-hooks-use
+```
+
+# Project Structure
+
+```
+│   
+├── index.html
+├── index.css
+└── script.js
+```
--- script.js
+++ script.js
@@ -1,96 +1,222 @@
-var countTask = 0;
 var inputTask = document.getElementById('input-task');
-var countUpdate = document.getElementById('count');
+var countUpdateFooter = document.getElementById('count');
+var notCom = document.getElementById("notcom");
+var com = document.getElementById("com");
+let allTasksButton = document.getElementById("notifications__all");
+let remainingTasksButton = document.getElementById("notifications__remaining");
+let completeTasksButton = document.getElementById("notifications__complete");
+let clerAllTasksButton = document.getElementById("notifications__clear");
 
-document.querySelector('.search-box .fa-plus-circle').addEventListener('click', function(){
+let JSONresult = localStorage.getItem("taskArray");
+let tasks = JSON.parse(JSONresult);
+
+updateNonCompleteTask(tasks);
+updateCompleteTask(tasks);
+
+// *********      SHOW ALL TASKS BUTTON        **********
+allTasksButton.addEventListener("click", function () {
+     com.style.display = "block";
+     notCom.style.display = "block";
+});
+
+// *********      SHOW REAMAINING TASKS BUTTON        **********
+remainingTasksButton.addEventListener("click", function () {
+    com.style.display = "none";
+    notCom.style.display = "block";
+});
+
+// *********      SHOW COMPLETE TASKS BUTTON        **********
+completeTasksButton.addEventListener("click", function () {
+    com.style.display = "block";
+    notCom.style.display = "none";
+});
+
+// ************   TOTAL COUNT UPDATE   **********
+function countUpdate(tasks) {
+    let TotalTasksRemaining = 0;
+
+    if(tasks != null){
+        for (let task of tasks) {
+          if (!task.complete) {
+            TotalTasksRemaining++;
+          }
+        }
+    }
+    
+    countUpdateFooter.innerHTML = TotalTasksRemaining;
+}
 
 
-    if(inputTask.value === "") {
+// ************   INCOMPLETED TASKS   **********
+function updateNonCompleteTask(tasks){
+
+    countUpdate(tasks);
+    
+    let child = document.querySelectorAll("#notcom div");
+    for (let i = 0; i < child.length; i++) {
+      child[i].remove();
+    }
+
+    for (let i = 0; tasks != null && i < tasks.length; i++) {
+      if (tasks[i].complete == false) {
+        let task = document.createElement("div");
+        task.classList.add("task");
+        task.innerHTML = tasks[i].text;
+
+        let del = document.createElement("i");
+        del.classList.add("fas");
+        del.classList.add("fa-trash");
+        task.append(del);
+
+        del.addEventListener('click', function(){
+            tasks.splice(i,1);
+            let tasksToJSON = JSON.stringify(tasks);
+            localStorage.setItem("taskArray", tasksToJSON);
+            updateNonCompleteTask(tasks);
+        })
+
+        let check = document.createElement("i");
+        check.classList.add("fas");
+        check.classList.add("fa-check");
+        task.append(check);
+        
+        check.addEventListener("click", function () {
+            let CompleteTask = {
+              text: tasks[i].text,
+              complete: true,
+            };
+          tasks.splice(i, 1);
+          tasks.unshift(CompleteTask);
+          let tasksToJSON = JSON.stringify(tasks);
+          localStorage.setItem("taskArray", tasksToJSON);
+          updateCompleteTask(tasks);
+          updateNonCompleteTask(tasks);
+          
+        });
+
+        notCom.append(task);
+      }
+    }
+}
+
+// ************   COMPLETED TASKS   **********
+function updateCompleteTask(tasks){
+    countUpdate(tasks);
+
+    let child = document.querySelectorAll("#com div");
+    for (let i = 0; i < child.length; i++) {
+      child[i].remove();
+    }
+
+    for (let i = 0; tasks != null && i < tasks.length; i++) {
+      if (tasks[i].complete == true) {
+        let task = document.createElement("div");
+        task.classList.add("task");
+        task.innerHTML = tasks[i].text;
+
+        let del = document.createElement("i");
+        del.classList.add("fas");
+        del.classList.add("fa-trash");
+        task.append(del);
+
+        del.addEventListener("click", function () {
+          tasks.splice(i, 1);
+          let tasksToJSON = JSON.stringify(tasks);
+          localStorage.setItem("taskArray", tasksToJSON);
+          updateCompleteTask(tasks);
+        });
+
+        com.append(task);
+      }
+    }
+
+}
+
+
+
+
+// **************     DELETE ALL TASKS   *************
+clerAllTasksButton.addEventListener("click", function () {
+  localStorage.clear();
+
+  let childs = document.querySelectorAll("#com div");
+  for (let child of childs) {
+    child.remove();
+  }
+  childs = document.querySelectorAll("#notcom div");
+  for (let child of childs) {
+    child.remove();
+  }
+  countUpdateFooter.innerHTML = 0;
+});
+
+
+
+// *********    Input by Plus Icon *************
+  document.querySelector(".search-box .fa-plus-circle").addEventListener("click", function () {
+      if (inputTask.value === "") { 
         alert("please enter some task");
         return;
-    }
+      }
 
-    countTask++;
-        var task = $('<div class="task"></div').text(inputTask.value);
-        var del = $('<i class="fas fa-trash"></i>').click(function(){
-            var ev = $(this).parent();
-            ev.fadeOut(function(){
-                ev.remove();
+      // NEW Task
+      let task = {
+        text: inputTask.value,
+        complete: false,
+      };
 
-                // To count numbers of task remaning
-                countTask--;
-                countUpdate.innerHTML = countTask;
-            });
-        });
-        var check = $('<i class="fas fa-check"></i>').click(function(){
-            var ev = $(this).parent();
-            ev.fadeOut(function(){
-                $('.com').append(ev);
-                ev.fadeIn();
-            });
-            $(this).remove();
-            // To count numbers of task remaning
-            countUpdate.innerHTML = countTask;
-        });
+      let JSONString = localStorage.getItem("taskArray");
+      let tasksArray = JSON.parse(JSONString);
 
-        task.append(del, check);
-        $('.notcom').append(task);
-        inputTask.value="";
+      if (tasksArray == null) {
+        tasksArray = [];
+      }
+      tasksArray.unshift(task);
 
-        // To count numbers of task remaning
-        countUpdate.innerHTML = countTask;
+      let tasksToJSON = JSON.stringify(tasksArray);
+      localStorage.clear();
+      // Tasks store in local Storage in the form of JSON
+      localStorage.setItem("taskArray", tasksToJSON);
 
-});
+      let JSONresult = localStorage.getItem("taskArray");
+      let tasks = JSON.parse(JSONresult);
+      
+      inputTask.value = "";
+
+      updateNonCompleteTask(tasks);
+
+    });
 
 
-// clear all tasks
-
-document.getElementById('clear-complete').addEventListener('click', function(){
-    var tasks = document.querySelectorAll('.task');
-    for(let i of tasks) {
-        i.remove();
-    }
-    countTask = 0;
-    countUpdate.innerHTML = countTask;
-});
-
-
-// for taking input by enter key 
-
+// *********    Input by Enter Key  *************  
 inputTask.addEventListener("keyup", function(event){
 
     if(event.keyCode == 13 && inputTask.value != "") {
-        // alert("hey");
+      // NEW TASK
+      let task = {
+        text: inputTask.value,
+        complete: false,
+      };
 
-        countTask++;
-        var task = $('<div class="task"></div').text(inputTask.value);
-        var del = $('<i class="fas fa-trash"></i>').click(function(){
-            var ev = $(this).parent();
-            ev.fadeOut(function(){
-                ev.remove();
+      let JSONString = localStorage.getItem("taskArray");
+      let tasksArray = JSON.parse(JSONString);
 
-                // To count numbers of task remaning
-                countTask--;
-                countUpdate.innerHTML = countTask;
-            });
-        });
-        var check = $('<i class="fas fa-check"></i>').click(function(){
-            var ev = $(this).parent();
-            ev.fadeOut(function(){
-                $('.com').append(ev);
-                ev.fadeIn();
-            });
-            $(this).remove();
-            // To count numbers of task remaning
-            countUpdate.innerHTML = countTask;
-        });
+      if (tasksArray == null) {
+        tasksArray = [];
+      }
+      tasksArray.unshift(task);
 
-        task.append(del, check);
-        $('.notcom').append(task);
-        inputTask.value = "";
+      let tasksToJSON = JSON.stringify(tasksArray);
+      localStorage.clear();
+      localStorage.setItem("taskArray", tasksToJSON);
 
-        // To count numbers of task remaning
-        countUpdate.innerHTML = countTask;
+      // Tasks store in local Storage in the form of JSON
+      let JSONresult = localStorage.getItem("taskArray");
+      let tasks = JSON.parse(JSONresult);
+
+      inputTask.value = "";
+
+      updateNonCompleteTask(tasks);
     }
 });
 
